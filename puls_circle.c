17174b436974215b575fca168c6580945a8124b8
LTC-117 <lucastentoni@gmail.com>
2024-10-08 20:07:52 -0300

    Circle animation

---
+++ puls_circle.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+int main(){
+    int r;
+
+    printf("insert the radius of your sphere: ");
+    scanf("%d", &r);
+
+    int d = 2*r;
+
+    do{
+        for(int x=0; x<=d; x++){
+            for(int y=0; y<=d; y++){
+                for(int z=0; z<=d; z++){
+                    if(sqrt(pow(x-r, 2) + pow(y-r, 2) + pow(z-r, 2)) < r){
+                        printf(" .");
+                    }
+                    else{
+                        printf("  ");
+                    }
+                }
+                printf("\n");
+            }
+            printf("\n");
+            system("cls"); //Clears the console screen for every new layer of the sphere
+        }
+    } while(time(NULL));
+
+    return 0;
+}
